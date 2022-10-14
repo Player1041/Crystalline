d58048e8975584dc5a308fd7f039b8ebda5f1baa
Player <redacteddoesit@gmail.com>
2022-10-14 15:56:06 +0100

    adding maps

---
+++ maps/tiled/clouds.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="clouds" tilewidth="32" tileheight="32" spacing="1" tilecount="15" columns="5">
+ <image source="../../../../../Assets/maps/sky.png" width="164" height="98"/>
+</tileset>
---
+++ maps/tiled/grassy terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="grassy terrain" tilewidth="32" tileheight="32" spacing="1" tilecount="1" columns="1">
+ <image source="../../../../../Assets/maps/grassy terrain.png" width="32" height="32"/>
+</tileset>
