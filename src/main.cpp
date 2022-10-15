0f99eb5e305fdbca210d06f3e259db6a2fbd91c5
Player <redacteddoesit@gmail.com>
2022-10-15 20:33:49 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,3 @@
-/*#include <QuickGame.hpp>
-
-int main(int argc, char** argv){
-    if(QuickGame_Init() < 0)
-        return 1;
-
-    QGTexture_t characterTex = QuickGame_Texture_Load("./sprites/character/life.png", 0, 0);
-    QGSprite_t characterSprite = QuickGame_Sprite_Create_Alt(240, 136, 18, 16, characterTex);
-
-    QuickGame_Graphics_Set2D();
-
-    while(QuickGame_Running()){
-        QuickGame_Graphics_Start_Frame();
-
-        QuickGame_Graphics_Clear();
-
-        QuickGame_Sprite_Draw(characterSprite);
-
-        QuickGame_Graphics_End_Frame(true);
-    }
-
-    QuickGame_Sprite_Destroy(&characterSprite);
-    QuickGame_Texture_Destroy(&characterTex);
-
-    QuickGame_Terminate();
-    return 0;
-}
-*/
-
 #include <QuickGame.hpp>
 
 using namespace QuickGame;
@@ -43,8 +14,8 @@ int main(int argc, char** argv){
 
     QGCamera2D camera = {
         {x = 0, y = 0},
-        rotation = 0f
-    }
+        rotation = 0f,
+    };
 
     set_camera(camera)
 
@@ -56,7 +27,7 @@ int main(int argc, char** argv){
         sprite.draw();
 
         end_frame(true);
-    }
+    };
 
     QuickGame::terminate();
     return 0;
