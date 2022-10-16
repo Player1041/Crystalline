89986950f7fafd9647f39503f500c0e1ee9e8d2b
Player <redacteddoesit@gmail.com>
2022-10-16 21:05:12 +0100

    added title screen

---
+++ src/title.hpp
@@ -0,0 +1,11 @@
+#include <QuickGame.hpp>
+
+using namespace QuickGame;
+using namespace QuickGame::Graphics;
+using namespace QuickGame::Input;
+using namespace QuickGame::Audio;
+
+/*
+draw an image, make it swap to level.hpp when PSP_CTRL_CROSS is pressed
+play a noise when PSP_CTRL_CROSS is played
+*/
