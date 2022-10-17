524bb5fc98d07083ed976fd7ed44321ddd0544c1
Player <redacteddoesit@gmail.com>
2022-10-17 17:44:07 +0100

    added screens

--- src/build/README.md
+++
@@ -1,2 +0,0 @@
-# game-assets
- 
--- src/title.hpp
+++ src/title.hpp
@@ -7,5 +7,14 @@ using namespace QuickGame::Audio;
 
 /*
 draw an image, make it swap to level.hpp when PSP_CTRL_CROSS is pressed
-play a noise when PSP_CTRL_CROSS is played
+play a noise when PSP_CTRL_CROSS is pressed; src/build/sounds/startup.wav
 */
+
+void title() {
+    Sprite title({0,0}, {480, 288}, {"sprites/title.png", 0, 0});
+    title.layer = -1;
+    title.transform.position.x = 240;
+    title.transform.position.y = 140;
+
+    return title.draw();
+    }
